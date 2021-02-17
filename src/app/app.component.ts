0d84b138fcbd5e4c36d5f7da45605db4c698f21c
saiprashanth606 <saiprashanthdyapa972@gmail.com>
2021-02-17 15:47:10 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -6,7 +7,7 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  //registrationForm =new FormGroup()
   get()
   {
     alert("welcome...!");
